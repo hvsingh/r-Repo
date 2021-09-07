3bccfdf3b2136e7f6d00da2a54563976c84ed825
Harsh Singh <harsh.vs@gmail.com>
2021-09-06 18:53:17 -0600

    Sept6- Finalize Week2 basics

--- Practice/Week2/LoadData.R
+++ Practice/Week2/LoadData.R
@@ -1,34 +1,35 @@
 #Loading data, R sucks with resolving relative paths, always give full paths
 #Read.table can be used to load tables
-lolo=read.table("NFLX.csv",header=T,na.strings="?")
+lolo=read.table("StockA.csv",header=T,na.strings="?")
 
 #prefer to use read.csv if using CSV
-NFLX=read.csv("/Users/sonalsingh/Documents/Harsh/DataAnalytics/r-Repo/Practice/Week2/NFLX.csv",header=T,na.strings="?")
+StockA=read.csv("/FULLPATH/r-Repo/Practice/Week2/StockA.csv",header=T,na.strings="?")
 
 #fix can be used to view the data in columns but my R on mac, does not like this command
-#fix(NFLX)
+#fix(StockA)
 
 #get an idea of the data volume by looking at number of rows & columns
-dim(NFLX)
+dim(StockA)
 #Also you can view the headers (of the columns)
-names(NFLX)
+names(StockA)
 
 #plot and identify
-plot(NFLX$Open)
-identify(NFLX$Open)
+plot(StockA$Open)
+identify(StockA$Open)
 
 #reformat the date
-NFLX$Date = as.Date(NFLX$Date, format="%Y-%m-%d")
-summary(NFLX)
+StockA$Date = as.Date(StockA$Date, format="%Y-%m-%d")
+summary(StockA)
 
 #prefer to use read.csv if using CSV
-TSLA=read.csv("/Users/sonalsingh/Documents/Harsh/DataAnalytics/r-Repo/Practice/Week2/TSLA.csv",header=T,na.strings="?")
+#StockB=read.csv("/FULLPATH/r-Repo/Practice/Week2/StockB.csv",header=T,na.strings="?")
 #reformat the date
-TSLA$Date = as.Date(TSLA$Date, format="%Y-%m-%d")
-summary(TSLA)
+StockB$Date = as.Date(StockB$Date, format="%Y-%m-%d")
+summary(StockB)
 
 #find mean on the samples by close and months and mean
-tapply(NFLX$Close, months(NFLX$Date), mean)
+tapply(StockB$Close, months(StockB$Date), mean)
+tapply(StockA$Close, months(StockA$Date), mean)
 #find mean on the samples by close and months and mean
-tapply(TSLA$Volume, weekdays(TSLA$Date), mean)
+tapply(StockB$Volume, weekdays(StockB$Date), mean)
 
